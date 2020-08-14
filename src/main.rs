c68979642b0d0b8f45c4235bdc358dd4f6ab4889
Dominic Rutkowski <rutkowski.dominic@gmail.com>
2020-08-14 00:34:57 -0500

    Optimize file watcher
    * Sacrifice initial performance by looping through Spotify directory to find specific files to watch
    * Boost continuous performance by watching fewer files (handling fewer events)

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 extern crate notify;
 
 use notify::{Watcher, raw_watcher, RecursiveMode};
-use std::fs;
-use std::sync::mpsc;
+use std::{fs, sync::mpsc};
 
 mod config;
 mod spotify;
@@ -15,7 +14,17 @@ fn main() {
     // Set up file watcher
     let (tx, rx) = mpsc::channel();
     let mut watcher = raw_watcher(tx).expect("Something went wrong creating a file watcher...");
-    watcher.watch(spotify_dir, RecursiveMode::Recursive).expect("Something went wrong adding the Spotify installation directory to the file watcher...");
+    for child in fs::read_dir(spotify_dir).expect("Something went wrong reading the Spotify installation directory...") {
+        if let Ok(child) = child {
+            let mut path = child.path();
+            if path.is_dir() {
+                path.push("ad-state-storage.bnk");
+                if path.exists() {
+                    watcher.watch(path, RecursiveMode::NonRecursive).expect("Something went wrong adding the Spotify installation directory to the file watcher...");
+                }
+            }
+        }
+    }
 
     // Watch for file changes
     loop {
