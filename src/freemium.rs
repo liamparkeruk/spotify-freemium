1422f8503fb6ea82a59df13e7ef215a3b94f94d5
liamparkeruk <75989572+liamparkeruk@users.noreply.github.com>
2021-01-17 11:59:02 +0000

    Update freemium.rs
    Removed mute and volume resetting.

--- src/freemium.rs
+++ src/freemium.rs
@@ -7,10 +7,8 @@ use crate::spotify;
 /// After an event occurs, the following steps are taken:
 ///
 /// 1. Ensure that Spotify is open and that an ad is playing
-/// 2. Store the current volume and mute Spotify
-/// 3. Quit and reopen Spotify
-/// 4. Restore the volume to its original value
-/// 5. Start audio playback
+/// 2. Quit and reopen Spotify
+/// 3. Start audio playback
 pub fn handle_event() -> bool {
     let mut restarted = false;
     match spotify::is_open() {
@@ -19,23 +17,15 @@ pub fn handle_event() -> bool {
                 match spotify::is_playing_ad() {
                     Ok(playing_ad) => {
                         if playing_ad {
-                            let volume = match spotify::get_volume() {
-                                Ok(volume) => volume,
-                                Err(error) => {
+                            let Err(error) => {
                                     eprintln!("{}", error);
                                     0
                                 },
                             };
-                            if let Err(error) = spotify::mute() {
-                                eprintln!("{}", error);
-                            }
                             if let Err(error) = spotify::quit() {
                                 eprintln!("{}", error);
                             }
                             if let Err(error) = spotify::open() {
-                                eprintln!("{}", error);
-                            }
-                            if let Err(error) = spotify::set_volume(volume) {
                                 eprintln!("{}", error);
                             }
                             if let Err(error) = spotify::play() {
